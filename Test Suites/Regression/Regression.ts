408528d3e96abadcc9a233438fecd47257fe8679
AlexanderClawee <alexander.v@clawee.com>
2021-12-07 17:29:12 +0200

    First commit

---
+++ Test Suites/Regression/Regression.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>daec0a7a-8ad1-4ece-b71c-251f2b6dcf18</testSuiteGuid>
+   <testCaseLink>
+      <guid>ee15dd0a-cbf1-4d00-8e00-cd806dd3a569</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
